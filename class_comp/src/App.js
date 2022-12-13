06cdcca02b7a76e0e39493b494c6784d5e0785c5
s_h_a_m_i_r <shahshamu164@gmail.com>
2022-12-13 22:29:17 +0500

    first commit

---
+++ class_comp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HelloWorld from "./components/helloWorld.jsx"
+
+function App() {
+  return (
+   <>
+    <HelloWorld />
+   </>
+  );
+}
+
+export default App;
---
+++ class_comp/src/components/helloWorld.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import  { Component } from 'react';
+import "./helloWorld.css"
+
+class helloWorld extends Component {
+    state = {  } 
+    render() { 
+        return ( <h1> hello world</h1>
+            
+        );
+    }
+}
+ 
+export default helloWorld;
